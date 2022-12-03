b565cdb4b515d01c7a4389148b9a3143c8706023
Ivan <batchev.ivan@yandex.ru>
2022-12-04 00:58:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import './App.css'
 
+class SwapiService {
+  _apiBase = 'https://swapi.dev/api/'
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`)
+
+    return await res.json()
+  }
+
+  getAllPeople() {
+    return this.getResource('/people/')
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+}
+
+const examplaApi = new SwapiService()
+const body = examplaApi.getPerson(24)
+body.then((res) => {
+  console.log(res)
+})
+
 export default class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello World!!!</h1>
-      </div>
-    )
+    return <div className="App"></div>
   }
 }
